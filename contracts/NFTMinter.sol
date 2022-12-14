f01806fc2c81cbd13a4359065d635e53e86ff7f8
ShanksDLAw <aabduljalaal@yahoo.com>
2022-12-14 14:09:18 +0100

    second commit

--- contracts/NFTMinter.sol
+++ contracts/NFTMinter.sol
@@ -24,6 +24,45 @@ pragma solidity 0.8.17;
 import "erc721a/contracts/ERC721A.sol"; 
 //Importing the ERC721A standard
 
-contract NFTMinter {
-    
+contract NFTMinter is ERC721A {
+
+    // This constructor initialising the NFT name and its symbol
+    constructor() ERC721A("Titan","Titan") {}
+     function mint(uint256 quantity) external onlyOwner {
+        // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
+        _mint(msg.sender, quantity);
+    }
+
+    /**
+     * This struct has the details of each token holder, which includes
+     * Owner address (it is an address)
+     * time stamp of when it was burned ()
+     * checks whether it has been burned or not
+     * 
+     * */
+    struct Token_Ownership {
+
+        address Owner_address; //checks the owners address
+        uint64 startTimeStamp; //checks the start timestamp, Got no clue why it is a uint64
+        bool burned; //checks whether it is burned
+
+    }
+
+
+/**
+ * We will now create another struct called address data (I got the idea from Alchemy)
+ * https://www.alchemy.com/blog/erc721-vs-erc721a-batch-minting-nfts
+ * this should help us through.
+ */
+
+struct Address_Data{
+    uint128 balance; // this tracks the user balance of the user
+    uint128 numberMinted; //tracks the number the user has minted
+}
+
+//This is the mint function 
+
+
+
+
 }
