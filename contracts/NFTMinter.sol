85261294a749436407d63a0ba85c44c155ef58fd
ShanksDLAw <aabduljalaal@yahoo.com>
2022-12-14 12:20:24 +0100

    second commit

---
+++ contracts/NFTMinter.sol
@@ -0,0 +1,29 @@
+/**
+ * The purpose of this project is to attempt to build something like Skillshare and coursera
+ * however it might not be exact, but the aim is provide a means for people to host educational content
+ * Enabling for countries with shitty educational content
+ * In Johnny in trust (I pray this project wont be too ball wrenching)
+ * This .sol file handles the NFT minting, which will allow people to Mint the projects NFT
+ * this NFT will safe as a gateway for educationalist and learners
+ * It will be based on ERC721A standard and I'll be doing some research and when I'm stuck. I'll research some more then run to 
+ * In Johnny we trust
+ * I'll be writing this code like I'm sharing it with people not used to coding with comments and other stuff
+ * Which will help in documentation as well.
+ * To utilise the ERC721A standard you can get the details about this for the standard
+ * https://chiru-labs.github.io/ERC721A/#/ and follow the rules
+ */
+
+
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+import "erc721a/contracts/ERC721A.sol"; 
+//Importing the ERC721A standard
+
+contract NFTMinter {
+    
+}
